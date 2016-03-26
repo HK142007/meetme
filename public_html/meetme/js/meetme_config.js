4a23837e4534eeeb87ad8f1d8179b4184c28cc0d
Anton Raharja <antonrd@gmail.com>
2016-03-27 03:54:02 +0700

    breakdown meetme.js

---
+++ public_html/meetme/js/meetme_config.js
@@ -0,0 +1,16 @@
+var server = "wss://meetme.id/gateway";
+var iceServers = [
+	{"urls": "stun:stun.meetme.id:443"},
+	{"urls": "turn:turn.meetme.id:443?transport=tcp", "credential": "meetme", "username": "meetme"}
+];
+var videoBoxWidth = 160;
+var videoBoxHeight = 120;
+var videoBoxLargeWidth = "100%";
+var videoBoxLargeHeight = "auto";
+var maxVideoBox = 17;
+var maxBitRate = 64000;
+var defaultResolution = "lowres";	// lowres, stdres, hires
+var debugLevel = "all"
+var audioCodec = "opus";
+var videoCodec = "vp9";
+var windowTitle = "meetme.id";
---
+++ public_html/meetme/js/meetme_lang.js
@@ -0,0 +1,55 @@
+//var labelEnterRoom = "Enter room number";
+var labelEnterRoom = "Masukkan nomor ruangan";
+
+//var labelDisplayName = "Enter your name";
+var labelDisplayName = "Masukkan nama anda";
+
+//var labelNoRemoteVideo = "No remote video available";
+//var labelNoRemoteVideo = "Tidak ada video";
+var labelNoRemoteVideo = "<img src='../images/novideo.jpg' width='"+videoBoxWidth+"' height='"+videoBoxHeight+"'>";
+
+//var labelNoWebcam = "No webcam available";
+//var labelNoWebcam = "Tidak ada webcam";
+var labelNoWebcam = "<img src='../images/novideo.jpg' width='"+videoBoxWidth+"' height='"+videoBoxHeight+"'>";
+
+//var lebelSpeaking = "Currently speaking";
+//var lebelSpeaking = "Sedang berbicara";
+var labelSpeaking = "<img src='../images/speaking.png' width='"+videoBoxWidth+"' height='"+videoBoxHeight+"'>";
+
+//var labelNoWebRTC = "No WebRTC support available";
+var labelNoWebRTC = "Tidak ada dukungan WebRTC";
+
+//var labelStartPublishing = "Start publishing";
+var labelStartPublishing = "Masuk";
+
+var labelStopPublishing = "<i class='glyphicon glyphicon-off'></i>";
+
+//var labelRoom = "Room";
+var labelRoom = "Ruangan";
+
+//var labelRoomNumber = "Room #";
+var labelRoomNumber = "Ruangan #";
+
+//var labelInvalidRoomNumber = "Room number is 1-10 digits only";
+var labelInvalidRoomNumber = "Nomor ruangan antara 1-10 digit";
+
+//var labelRoomNumericOnly = "Room number is numeric only";
+var labelRoomNumericOnly = "Ruangan hanya angka saja";
+
+//var labelDisplayNameAlphanumeric = "Name is alphanumeric and spaces only";
+var labelDisplayNameAlphanumeric = "Nama hanya boleh alphanumeric dan spasi saja";
+
+// labelMuteOn means mute is OFF, publisher will send the audio
+var labelMuteOn = "<i class='glyphicon glyphicon-volume-up'></i>";
+
+// labelMuteOff means mute is actually ON and publisher will NOT send the audio
+var labelMuteOff = "<i class='glyphicon glyphicon-volume-off'></i>";
+
+// labelPauseOn means mute is OFF, publisher will send the video
+var labelPauseOn = "<i class='glyphicon glyphicon-facetime-video'></i>";
+
+// labelPauseOff means mute is actually ON and publisher will NOT send the video
+var labelPauseOff = "<i class='glyphicon glyphicon-pause'></i>";
+
+//var labelConfirmExit = "Are you sure you want to exit ?";
+var labelConfirmExit = "Yakin ingin keluar dari ruang meeting ?";
