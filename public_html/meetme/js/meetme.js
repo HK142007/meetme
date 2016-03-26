b667488ec7ac8a44180912a6cc777df1adc8456c
Anton Raharja <antonrd@gmail.com>
2016-03-26 21:01:13 +0700

    resize small videobox and increase the limit of videobox to 17

--- public_html/meetme/js/meetme.js
+++ public_html/meetme/js/meetme.js
@@ -3,11 +3,11 @@ var iceServers = [
 	{"urls": "stun:stun.meetme.id:443"},
 	{"urls": "turn:turn.meetme.id:443?transport=tcp", "credential": "meetme", "username": "meetme"}
 ];
-var videoBoxWidth = 160;
-var videoBoxHeight = 120;
+var videoBoxWidth = 146;
+var videoBoxHeight = 110;
 var videoBoxLargeWidth = "100%";
 var videoBoxLargeHeight = "auto";
-var maxVideoBox = 13;
+var maxVideoBox = 17;
 var maxBitRate = 64000;
 var defaultResolution = "lowres";	// lowres, stdres, hires
 var debugLevel = "all"
